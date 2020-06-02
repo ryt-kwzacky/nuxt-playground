85df39c8343d72692a1746741823a3cf28474a73
RyutoKawasaki <various.use.0416@gmail.com>
2020-06-02 13:00:58 +0900

    install vuelidate

--- pages/vuelidate/index.vue
+++ pages/vuelidate/index.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>nuxt-playground</div>
+  <div></div>
 </template>
 
 <script lang="ts">
+// import { Validation } from 'vuelidate'
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  computed: {}
+})
 </script>
 
 <style lang="scss" scoped></style>
