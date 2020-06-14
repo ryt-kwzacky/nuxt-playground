bd1dd20fbc63774e39282f22578438023f89ac7c
RyutoKawasaki <various.use.0416@gmail.com>
2020-06-14 14:29:15 +0900

    add enum page

---
+++ pages/enum/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>{{ enumTest() }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+enum Test {
+  Test1 = 0,
+  Test2 = 1,
+  Test3 = 2
+}
+
+export default Vue.extend({
+  methods: {
+    enumTest() {
+      return Test.Test1
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/v-for/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-for="(test, index) in tests" :key="index">
+      <div v-if="index === 0">
+        <p>this is the first element</p>
+      </div>
+      <div v-if="index !== 0">
+        <p>this is not first element</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return { tests: ['test1', 'test2', 'test3'] }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
