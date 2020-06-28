14c5b9100442f935f2ac538d3d71ac7228b7ca3b
RyutoKawasaki <various.use.0416@gmail.com>
2020-06-28 19:15:52 +0900

    rename

--- pages/firebase/firestore/where.vue
+++ pages/firebase/firestore/where.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>where</div>
+  <div>
+    <h1>where</h1>
+    <button @click="getTest">click</button>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { db } from '~/plugins/firebase'
 
-export default Vue.extend({})
+export default Vue.extend({
+  methods: {
+    async getTest() {
+      const testRef = db.collection('test')
+      const test = await testRef.get()
+      console.log(test)
+    }
+  }
+})
 </script>
 
 <style lang="scss" scoped></style>
